00bdc31e351a80893439f2b67822e4da3a801e7d
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-10-02 11:19:51 -0300

    deal

--- js/script.js
+++ js/script.js
@@ -30,3 +30,32 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
   });
+
+  //-------Count----------->
+  let countDate = new Date('october 10, 2021 00:00:00').getTime();
+
+  function coutDown(){
+    let now = new Date().getTime();
+
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+  }
+
+  setInterval(function(){
+      coutDown();
+},1000)
+
